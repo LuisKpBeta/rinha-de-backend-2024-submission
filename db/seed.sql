4e751e48d505082c88abbd12d1473af90c945968
Luis <lu.ca.oliveira41@gmail.com>
2024-02-05 22:36:35 -0400

    chore: postgres base config

---
+++ db/seed.sql
@@ -0,0 +1,27 @@
+CREATE TABLE IF NOT EXISTS customers (
+    id serial PRIMARY KEY,
+    name varchar(50) NOT NULL,
+    balance integer NOT NULL DEFAULT 0,
+    account_limit integer NOT NULL DEFAULT 0
+);
+
+CREATE TABLE IF NOT EXISTS transactions (
+  id serial PRIMARY KEY,
+  value integer NOT NULL,
+  description varchar(11) NOT NULL, 
+  type varchar(1) NOT NULL,
+  customer_id int NOT NULL,
+  FOREIGN KEY (customer_id) REFERENCES customers (id)
+);
+
+DO $$
+BEGIN
+  INSERT INTO customers (name, account_limit)
+  VALUES
+    ('first', 1000 * 100),
+    ('second', 800 * 100),
+    ('third', 10000 * 100),
+    ('fourth', 100000 * 100),
+    ('fifth', 5000 * 100);
+END; $$
+
