aed52c1d608823f2a9d095008bfa0984457be17b
Luis <lu.ca.oliveira41@gmail.com>
2024-02-21 23:25:14 -0400

    refactor: moving balance update only for database

--- src/Database.cs
+++ src/Database.cs
@@ -62,72 +62,72 @@ public class Database
     };
     return estatements;
   }
-  public async Task<bool> InsertTransaction(Transacao newTransaction, NpgsqlTransaction? t = null)
+  public async Task<bool> InsertTransaction(Transacao newTransaction, int clientId, NpgsqlTransaction? t = null)
   {
     using var command = _conn.CreateCommand();
     command.CommandText = "INSERT INTO transactions (value, description, type, customer_id, operation_date) VALUES (@value, @description,@type, @customer_id, @date)";
     command.Parameters.AddWithValue("@value", newTransaction.Valor!);
     command.Parameters.AddWithValue("@description", newTransaction.Descricao!);
     command.Parameters.AddWithValue("@type", newTransaction.Tipo!);
-    command.Parameters.AddWithValue("@customer_id", newTransaction.Client!.Id);
+    command.Parameters.AddWithValue("@customer_id", clientId);
     command.Parameters.AddWithValue("@date", DateTime.Now);
     await command.PrepareAsync();
     int rowsAffected = await command.ExecuteNonQueryAsync();
     command.Parameters.Clear();
     return rowsAffected > 0;
   }
-  public async Task<bool> UpdateClient(Cliente cliente)
+  public async Task<(bool, int, int)> UpdateClient(int clienteId, int valor)
   {
     using var command = _conn.CreateCommand();
-    command.CommandText = "UPDATE customers SET balance = @novoSaldo WHERE id = @id;";
-    command.Parameters.AddWithValue("novoSaldo", cliente.Saldo);
-    command.Parameters.AddWithValue("id", cliente.Id);
+    command.CommandText = "UPDATE customers SET balance = @valor + balance WHERE id = @id and (balance + @valor + account_limit)>=0  RETURNING balance, account_limit ;";
+    command.Parameters.AddWithValue("valor", valor);
+    command.Parameters.AddWithValue("id", clienteId);
     await command.PrepareAsync();
-    int rowsAffected = await command.ExecuteNonQueryAsync();
-    return rowsAffected > 0;
+    using var reader = await command.ExecuteReaderAsync();
+    while (reader.Read())
+    {
+      int saldo = reader.GetInt32(0);
+      int limit = reader.GetInt32(1);
+      // bool success = reader.GetBoolean(2);
+      return (true, saldo, limit);
+    }
+    return (false, 0, 0);
   }
 
   public async Task<TransactionResult> ProcessTransaction(int clientId, Transacao newTransaction)
   {
 
-    // await using var t = await _conn.BeginTransactionAsync();
+    await using var t = await _conn.BeginTransactionAsync();
     var newT = new TransactionResult();
-    try
+
+    var valor = newTransaction.GetValue();
+
+    var command = new NpgsqlCommand("select pg_advisory_lock(id) FROM customers WHERE id = @id", _conn);
+    command.Parameters.AddWithValue("@id", clientId);
+    await command.ExecuteScalarAsync();
+
+    // newTransaction.Client = client;
+
+    var (ok, saldo, limit) = await UpdateClient(clientId, valor);
+
+    command = new NpgsqlCommand("select pg_advisory_unlock(id) FROM customers WHERE id = @id", _conn);
+    command.Parameters.AddWithValue("@id", clientId);
+    await command.ExecuteScalarAsync();
+    if (!ok)
     {
-      var client = await GetClientById(clientId)!;
-      var command = new NpgsqlCommand("select pg_advisory_lock(id) FROM customers WHERE id = @id", _conn);
-      command.Parameters.AddWithValue("@id", clientId);
-      await command.ExecuteScalarAsync();
-
-      newTransaction.Client = client;
-      var saldoOk = newTransaction.CanBeMade();
-      if (!saldoOk)
-      {
-        newT.Sucesso = false;
-        return newT;
-      }
-      newTransaction.DoOperation();
-      await UpdateClient(newTransaction.Client!);
-
-
-      command = new NpgsqlCommand("select pg_advisory_unlock(id) FROM customers WHERE id = @id", _conn);
-      command.Parameters.AddWithValue("@id", clientId);
-      await command.ExecuteScalarAsync();
-
-      await InsertTransaction(newTransaction);
-
-
-      newT.Limite = newTransaction.Client!.Limite;
-      newT.Saldo = newTransaction.Client.Saldo;
-      return newT;
-    }
-    catch (Exception er)
-    {
-      Console.WriteLine(er);
-      // await t.RollbackAsync();
       newT.Sucesso = false;
+      await t.RollbackAsync();
       return newT;
     }
+
+    var lockTransaction = new NpgsqlCommand("LOCK TABLE transactions IN EXCLUSIVE MODE", _conn);
+    await lockTransaction.ExecuteScalarAsync();
+    await InsertTransaction(newTransaction, clientId);
+    await t.CommitAsync();
+
+    newT.Limite = saldo;
+    newT.Saldo = limit;
+    return newT;
   }
 
   public bool ClientExists(int id)
