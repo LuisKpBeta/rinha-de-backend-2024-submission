12ec513fec2517abe431bbe3ca79854d1b8ff190
Luis <lu.ca.oliveira41@gmail.com>
2024-02-21 23:26:29 -0400

    refactor: simplifying models

--- src/Entities.cs
+++ src/Entities.cs
@@ -23,37 +23,20 @@ public class Transacao
 {
   public Cliente? Client { get; set; }
 
-  public int? Valor { get; set; }
+  public int Valor { get; set; }
   public string? Descricao { get; set; }
   public string? Tipo { get; set; }
 
   public DateTime RealizadaEm { get; set; }
 
-  public bool IsValid()
+  public int GetValue()
   {
-    var valueOk = Valor >= 0;
-    var descriptionOk = !string.IsNullOrEmpty(Descricao) && Descricao.Length >= 1 && Descricao.Length <= 10;
-    var typeOk = !string.IsNullOrEmpty(Tipo) && Tipo == "c" || Tipo == "d";
+    if (Tipo == "c")
+    {
+      return Valor!;
+    }
 
-    return valueOk && descriptionOk & typeOk;
-  }
-  public bool CanBeMade()
-  {
-    if (Tipo == "c")
-    {
-      return true;
-    }
-    var total = Client!.Saldo + Client!.Limite;
-    return Valor < total;
-  }
-  public void DoOperation()
-  {
-    if (Tipo == "c")
-    {
-      Client!.Saldo += Valor ?? 0;
-      return;
-    }
-    Client!.Saldo -= Valor ?? 0;
+    return Valor! * -1;
   }
 
 }
